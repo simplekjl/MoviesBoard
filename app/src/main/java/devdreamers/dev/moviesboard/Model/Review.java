e0d54420d6352090eb4c317fd8e2b1870a558ec5
Jose Luis Crisostomo <jl.cs606@gmail.com>
2016-05-07 23:52:52 -0400

    Fixed review Model

--- app/src/main/java/devdreamers/dev/moviesboard/Model/Review.java
+++ app/src/main/java/devdreamers/dev/moviesboard/Model/Review.java
@@ -10,6 +10,15 @@ public class Review {
     private String id;
     private String author;
     private String content;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
     public String getId() {
         return id;
