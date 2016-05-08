4485d520707cdeb6f25130eb3cb6be48d21f77f3
Jose Luis Crisostomo <jl.cs606@gmail.com>
2016-05-07 21:16:25 -0400

    Methods to get top rated and popular movies added

--- app/src/main/java/devdreamers/dev/moviesboard/Network/MoviesServiceAPI.java
+++ app/src/main/java/devdreamers/dev/moviesboard/Network/MoviesServiceAPI.java
@@ -1,6 +1,9 @@
 package devdreamers.dev.moviesboard.Network;
 
+import devdreamers.dev.moviesboard.Model.ResponseAPI;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 
 /**
@@ -11,8 +14,24 @@ import retrofit2.http.GET;
 public interface MoviesServiceAPI {
 
 
-    @GET("/3/movie/popular?api_key")
-    Call<>
+    /**
+     * Method to get the popular movies in the API
+     * @param api_key
+     * @return
+     */
+    @FormUrlEncoded
+    @GET("/3/movie/popular")
+    Call<ResponseAPI>   getPopularMovies(@Field("api_key") String api_key);
+
+
+    /**
+     * Method to get the top rated movies in the API
+     * @param api_key
+     * @return
+     */
+    @FormUrlEncoded
+    @GET("/3/movie/top_rated")
+    Call<ResponseAPI>   getTopRatedMovies(@Field("api_key") String api_key);
 
 
 }
