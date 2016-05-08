6f60ce2d7f0a19ff3d42558a18ab43fdbe9db3c2
Jose Luis Crisostomo <jl.cs606@gmail.com>
2016-05-07 21:09:05 -0400

    Initial commit

---
+++ app/src/main/java/devdreamers/dev/moviesboard/Network/MoviesService.java
@@ -0,0 +1,7 @@
+package devdreamers.dev.moviesboard.Network;
+
+/**
+ * Created by jlcs on 5/7/16.
+ */
+public class MoviesService {
+}
---
+++ app/src/main/java/devdreamers/dev/moviesboard/Network/MoviesServiceAPI.java
@@ -0,0 +1,18 @@
+package devdreamers.dev.moviesboard.Network;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ *
+ * API service interface, it's going to tke place once the retrofit has been created.
+ * Created by jlcs on 5/7/16.
+ */
+public interface MoviesServiceAPI {
+
+
+    @GET("/3/movie/popular?api_key")
+    Call<>
+
+
+}
